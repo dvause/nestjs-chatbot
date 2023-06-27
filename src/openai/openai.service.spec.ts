8befa15ee2c4de9335a615d44e67b74baceba38f
Dave Vause <dvause@gmail.com>
2023-06-26 21:11:25 -0400

    Fix openai test

--- src/openai/openai.service.spec.ts
+++ src/openai/openai.service.spec.ts
@@ -4,6 +4,7 @@ import * as dotenv from 'dotenv';
 import { OpenAIModule } from './openai.module';
 import * as process from 'process';
 import { OpenAIConfig } from './openai.interfaces';
+import { ChatCompletionRequestMessage } from 'openai';
 
 describe('OpenAIService', () => {
   let service: OpenAIService;
@@ -33,7 +34,19 @@ describe('OpenAIService', () => {
   });
 
   it('should return a chat response', async () => {
-    const response = await service.chat();
+    const messages: ChatCompletionRequestMessage[] = [];
+    const systemMessage: ChatCompletionRequestMessage = {
+      role: 'system',
+      content: 'Hello, I am a chatbot. I am here to help you.',
+    };
+    const userMessage: ChatCompletionRequestMessage = {
+      role: 'user',
+      content: 'Hello, World!',
+    };
+    messages.push(systemMessage);
+    messages.push(userMessage);
+
+    const response = await service.chat(messages);
     console.log(response);
     expect(response).toBeDefined();
   });
