9913292bb2e76e9bd2b654d27a07fa9d956a5ede
Dave Vause <dvause@redventures.com>
2023-06-27 15:20:41 -0400

    Fix tests

--- src/chat/chat.controller.spec.ts
+++ src/chat/chat.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ChatController } from './chat.controller';
+import { ChatService } from './chat.service';
+import { OpenAIService } from '../openai/openai.service';
 
 describe('ChatController', () => {
   let controller: ChatController;
@@ -7,6 +9,10 @@ describe('ChatController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ChatController],
+      providers: [
+        ChatService,
+        { provide: OpenAIService, useValue: OpenAIService },
+      ],
     }).compile();
 
     controller = module.get<ChatController>(ChatController);
--- src/chat/chat.service.spec.ts
+++ src/chat/chat.service.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ChatService } from './chat.service';
+import { OpenAIService } from '../openai/openai.service';
 
 describe('ChatService', () => {
   let service: ChatService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ChatService],
+      providers: [
+        ChatService,
+        { provide: OpenAIService, useValue: OpenAIService },
+      ],
     }).compile();
 
     service = module.get<ChatService>(ChatService);
