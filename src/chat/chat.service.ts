ec5c291c74f1b03cd413f617a6ebc3e221ef90cb
Dave Vause <dvause@gmail.com>
2023-07-12 22:46:56 -0400

    Langchain
      - Implement langchain openai chat
      - Implement BufferMemory for chat history.
      - Addl OpenAI config params.

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -1,46 +1,16 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { OpenAIService } from '../openai/openai.service';
-import {
-  ChatCompletionRequestMessage,
-  ChatCompletionRequestMessageRoleEnum,
-  ChatCompletionResponseMessage,
-} from 'openai';
 import { ChatMessageDto } from './dto/chat-message.dto';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class ChatService {
-  private readonly logger = new Logger(ChatService.name);
-  private messages: ChatCompletionRequestMessage[] = [];
-
   constructor(
     private readonly configService: ConfigService,
     private readonly openAIService: OpenAIService,
   ) {}
 
-  async chat(
-    chatMessageDto: ChatMessageDto,
-  ): Promise<ChatCompletionResponseMessage> {
-    // If this is the first message, add the system prompt.
-    if (this.messages.length === 0) {
-      const systemPrompt =
-        this.configService.get<string>('SYSTEM_PROMPT') ||
-        'You are a helpful assistant.';
-
-      const systemMessage: ChatCompletionRequestMessage = {
-        role: ChatCompletionRequestMessageRoleEnum.System,
-        content: systemPrompt,
-      };
-      this.messages.push(systemMessage);
-    }
-    const userMessage: ChatCompletionRequestMessage = {
-      role: ChatCompletionRequestMessageRoleEnum.User,
-      content: chatMessageDto.message,
-    };
-    this.messages.push(userMessage);
-    const response = await this.openAIService.chatCompletion(this.messages);
-    this.messages.push(response);
-    this.logger.log('messages', this.messages);
-    return response;
+  async chat(chatMessageDto: ChatMessageDto) {
+    return await this.openAIService.chatCompletion(chatMessageDto.message);
   }
 }
--- src/openai/openai.service.ts
+++ src/openai/openai.service.ts
@@ -1,32 +1,52 @@
-import { Injectable, Inject, Logger } from '@nestjs/common';
-import {
-  ChatCompletionRequestMessage,
-  ChatCompletionResponseMessage,
-  Configuration,
-  OpenAIApi,
-} from 'openai';
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import { ChatOpenAI } from 'langchain/chat_models/openai';
+import { ChainValues } from 'langchain/schema';
 import { OpenAIConfig } from './openai.interfaces';
+import {
+  ChatPromptTemplate,
+  HumanMessagePromptTemplate,
+  MessagesPlaceholder,
+  SystemMessagePromptTemplate,
+} from 'langchain/prompts';
+import { BufferMemory } from 'langchain/memory';
+import { ConversationChain } from 'langchain/chains';
 
 @Injectable()
 export class OpenAIService {
   private readonly logger = new Logger(OpenAIService.name);
-  private apiClient: OpenAIApi;
+  private readonly chat: ChatOpenAI;
+  private memory: BufferMemory = new BufferMemory({
+    returnMessages: true,
+    memoryKey: 'history',
+  });
 
   constructor(@Inject('OPENAI_CONFIG') private readonly config: OpenAIConfig) {
-    const configuration = new Configuration({ apiKey: this.config.apiKey });
-    this.apiClient = new OpenAIApi(configuration);
+    this.chat = new ChatOpenAI({
+      openAIApiKey: this.config.apiKey,
+      temperature: this.config.temperature,
+      modelName: this.config.modelName,
+    });
   }
 
-  async chatCompletion(
-    messages: ChatCompletionRequestMessage[],
-  ): Promise<ChatCompletionResponseMessage> {
+  async chatCompletion(userMessage: string): Promise<ChainValues> {
+    const systemMessage = this.config.systemMessage;
+
+    const chatPrompt: ChatPromptTemplate =
+      ChatPromptTemplate.fromPromptMessages([
+        SystemMessagePromptTemplate.fromTemplate(systemMessage),
+        new MessagesPlaceholder('history'),
+        HumanMessagePromptTemplate.fromTemplate('{text}'),
+      ]);
+
     try {
-      const chatCompletion = await this.apiClient.createChatCompletion({
-        model: 'gpt-3.5-turbo',
-        messages: messages,
-        temperature: 0.7,
+      const chain = new ConversationChain({
+        memory: this.memory,
+        prompt: chatPrompt,
+        llm: this.chat,
       });
-      return chatCompletion.data.choices[0].message;
+      return await chain.call({
+        text: userMessage,
+      });
     } catch (error) {
       if (error.response?.status) {
         this.logger.error(error.response.status);
@@ -37,22 +57,22 @@ export class OpenAIService {
     }
   }
 
-  async completion(prompt: string): Promise<string> {
-    try {
-      const completion = await this.apiClient.createCompletion({
-        model: 'text-davinci-003',
-        prompt: prompt,
-        temperature: 0.7,
-      });
-      // console.log(completion.data);
-      return completion.data.choices[0].text;
-    } catch (error) {
-      if (error.response?.status) {
-        this.logger.error(error.response.status);
-        this.logger.error(error.response.data);
-      } else {
-        this.logger.error(error.message);
-      }
-    }
-  }
+  // async completion(prompt: string): Promise<string> {
+  //   try {
+  //     const completion = await this.apiClient.createCompletion({
+  //       model: 'text-davinci-003',
+  //       prompt: prompt,
+  //       temperature: 0.7,
+  //     });
+  //     // console.log(completion.data);
+  //     return completion.data.choices[0].text;
+  //   } catch (error) {
+  //     if (error.response?.status) {
+  //       this.logger.error(error.response.status);
+  //       this.logger.error(error.response.data);
+  //     } else {
+  //       this.logger.error(error.message);
+  //     }
+  //   }
+  // }
 }
